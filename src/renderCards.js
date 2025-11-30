b1e0100c1dae3e8f6634dd2504d7b6caa235fc1c
Eugene <7zhhovalyg@mail.ru>
2025-12-01 02:20:46 +0300

    Fix API URL to include per_page parameter

--- src/renderCards.js
+++ src/renderCards.js
@@ -3,7 +3,7 @@ const USERNAME = "wrzdx";
 async function getUserRepos(username) {
   try {
     const response = await fetch(
-      `https://api.github.com/users/${username}/repos`
+      `https://api.github.com/users/${username}/repos?per_page=100`
     );
     if (!response.ok) {
       const errorText = await response.text();
